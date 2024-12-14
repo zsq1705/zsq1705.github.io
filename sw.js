d02d7f0e677df34f7fe0ee35015d98a175278a2d
zsq1705 <981328zsqqq.com>
2024-12-14 22:48:38 +0800

    Site updated: 2024-12-14 22:48:37

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1733753354851";
+const VERSION = "1734187703789";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
